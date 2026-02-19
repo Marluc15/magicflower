c84a0a035ba18da7255d7f4622f82f8ab2d7e49d
Luciano Mariani <luciano.mariani@r2u.it>
2026-02-19 11:15:10 +0100

    Deploy iniziale con Astro

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+// https://astro.build/config
+// export default defineConfig({});
+
+// astro.config.mjs
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://Marluc15.github.io/magicflower',
+  base: '/magicflower',
+});
